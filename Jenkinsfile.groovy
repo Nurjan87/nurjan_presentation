30355e7b68f05c02cea90b95c490aa93a613377e
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-05-04 21:15:57 -0500

    remove

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,6 +2,6 @@ node{
     stage("Go to Terraform"){
         // sh "ssh root@18.220.132.65 git clone https://github.com/Nurjan87/nurjan_presentation.git"
         sh "ssh root@18.220.132.65 cd nurjan_presentation && terraform init"
-        sh "ssh root@18.220.132.65 cd nurjan_presentation && terraform apply --auto-approve"
+        sh "ssh root@18.220.132.65 terraform apply --auto-approve"
     }
 }
