7a7a059d8e818d57582d6a826b575976a3950f65
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-05-04 21:10:01 -0500

    root

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,7 @@
 node{
     stage("Go to Terraform"){
-        sh "ssh ec2-user@18.220.132.65 https://github.com/Nurjan87/nurjan_presentation.git"
-        sh "ssh ec2-user@18.220.132.65 cd nurjan_presentation && terraform init"
-        sh "ssh ec2-user@18.220.132.65 cd nurjan_presentation && terraform apply"
+        sh "ssh root@18.220.132.65 https://github.com/Nurjan87/nurjan_presentation.git"
+        sh "ssh root@18.220.132.65 cd nurjan_presentation && terraform init"
+        sh "ssh root@18.220.132.65 cd nurjan_presentation && terraform apply"
     }
 }
