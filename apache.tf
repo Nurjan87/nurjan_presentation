95af51f9012149c583b92fea9c2cfd73864375db
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-05-04 20:47:01 -0500

    test

--- apache.tf
+++ apache.tf
@@ -3,11 +3,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami                         = ami-0019ef04ac50be30f
-  instance_type               = t2.micro
-  count                       = 1
-  key_name                    = ec2-nur
-  subnet_id                   = subnet-6e764b35
+  ami                         = "ami-0019ef04ac50be30f"
+  instance_type               = "t2.micro"
+  count                       = "1"
+  key_name                    = "ec2-nur"
+  subnet_id                   = "subnet-6e764b35"
   associate_public_ip_address = "true"
   
   user_data = "${file("userdata.sh")}"
