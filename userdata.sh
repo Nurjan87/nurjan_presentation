719e794b687899ddf35b2951efe04f976e83f3a5
Nurjan87 <47833945+Nurjan87@users.noreply.github.com>
2019-05-09 18:18:09 -0500

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 yum install httpd -y
-echo "hello class" >> /var/www/html/index.html
+echo "This is my project, I have written a simple terraform file that creates ec2_instance and installs apache then prints this message! :)" >> /var/www/html/index.html
 systemctl restart httpd
 systemctl enable httpd 
